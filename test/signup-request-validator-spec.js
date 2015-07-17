2560ad2be82a28e58e0b329b66bd8ec0dc8172ba
András Iványi <andras.ivanyi@gmail.com>
2015-07-17 11:01:23 +0200

    Test cases added to request validator and response builder.

---
+++ test/signup-request-validator-spec.js
@@ -0,0 +1,92 @@
+var chai = require("chai");
+var expect = chai.expect;
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var SignupRequestValidator = require("../util/signup-request-validator");
+chai.use(sinonChai);
+
+
+
+describe("signup-request-validator", function () {
+  var data;
+  beforeEach(function () {
+    data = {
+      name: 'aaa',
+      email: 'almafa@kortefa.com',
+      birthdate: '1971-01-01'
+    }
+  });
+
+  it("should return with a VALID result when every field is correct", function() {
+
+    var validator = new SignupRequestValidator();
+
+    var result = validator.validateRequest(data);
+
+    expect(result.isValid).to.be.true;
+
+  });
+
+  it("should return with an INVALID/MISSING result when name field is missing", function() {
+    delete data.name;
+    var validator = new SignupRequestValidator();
+
+    var result = validator.validateRequest(data);
+
+    expect(result.isValid).to.be.false;
+    expect(result.issues.length).to.be.equal(1);
+    expect(result.issues).to.contain({field: "name", issue:validator.issues.MISSING});
+
+  });
+
+  it("should return with an INVALID/MISSING result when email field is missing", function() {
+    delete data.email;
+    var validator = new SignupRequestValidator();
+
+    var result = validator.validateRequest(data);
+
+    expect(result.isValid).to.be.false;
+    expect(result.issues.length).to.be.equal(1);
+    expect(result.issues).to.contain({field: "email", issue:validator.issues.MISSING});
+
+  });
+
+  it("should return with an INVALID/MISSING result when birthdate field is missing", function() {
+    delete data.birthdate;
+
+    var validator = new SignupRequestValidator();
+    var result = validator.validateRequest(data);
+
+    expect(result.isValid).to.be.false;
+    expect(result.issues.length).to.be.equal(1);
+    expect(result.issues).to.contain({field: "birthdate", issue:validator.issues.MISSING});
+  });
+
+  it("should return with an INVALID/INVALID result when email field is invalid", function() {
+    data.email = "hello";
+
+    var validator = new SignupRequestValidator();
+    var result = validator.validateRequest(data);
+
+    expect(result.isValid).to.be.false;
+    expect(result.issues.length).to.be.equal(1);
+
+    expect(result.issues).to.contain({field: "email", issue:validator.issues.INVALID});
+
+  });
+
+  it("should return with an INVALID/INVALID result when age is < 18", function() {
+    //should be working after 18 years as well...
+    data.birthdate = new Date();
+
+    var validator = new SignupRequestValidator();
+    var result = validator.validateRequest(data);
+
+    expect(result.isValid).to.be.false;
+    expect(result.issues.length).to.be.equal(1);
+
+    expect(result.issues).to.contain({field: "birthdate", issue:validator.issues.INVALID});
+
+  });
+
+});
---
+++ test/signup-response-spec.js
@@ -0,0 +1,60 @@
+var chai = require("chai");
+var expect = chai.expect;
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var SignupResponse = require("../util/signup-response");
+chai.use(sinonChai);
+
+
+
+describe("signup-response", function () {
+
+
+  it("should generate a response if no setup is provided", function() {
+      var response = new SignupResponse();
+
+      var resp = response.build();
+
+      expect(resp.status).to.be.equal(1);
+  });
+
+  it("should generate a response with data if data is provided", function() {
+      var response = new SignupResponse();
+
+      var resp = response.setData("hello").build();
+
+      expect(resp.status).to.be.equal(1);
+      expect(resp.data).to.be.equal("hello");
+  });
+
+  it("should generate a response with error if error is provided", function() {
+      var response = new SignupResponse();
+
+      var resp = response.setError("hello").build();
+
+      expect(resp.status).to.be.equal(1);
+      expect(resp.error).to.be.equal("hello");
+
+  });
+
+  it("should generate a response with error if error AND data is provided", function() {
+      var response = new SignupResponse();
+
+      var resp = response.setError("hello").setData("whatsup").build();
+
+      expect(resp.status).to.be.equal(1);
+      expect(resp.error).to.be.equal("hello");
+      expect(resp.data).not.to.exist;
+
+  });
+
+  it("should generate a response with the given status code if it is provided", function() {
+      var response = new SignupResponse();
+
+      var resp = response.setStatus(2).build();
+
+      expect(resp.status).to.be.equal(2);
+
+  });
+
+});
