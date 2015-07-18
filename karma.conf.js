adf9e860c6f1c20ccef55e14c52554dbba6ca6d6
András Iványi <andras.ivanyi@gmail.com>
2015-07-18 12:01:04 +0200

    Karma test coverage reporter was added to the project.

--- karma.conf.js
+++ karma.conf.js
@@ -36,14 +36,27 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      
+      './public/js/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage'],
 
+
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'coverage/karma/',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    },
 
     // web server port
     port: 9876,
